5f3398eef5bf696bc032ae16893748a853a55618
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-10-18 21:48:22 -0500

    list

--- src/components/CardProducts.js
+++ src/components/CardProducts.js
@@ -8,142 +8,125 @@ const CardProducts = (props) => {
     const { id, name, cantidad, price, image } = props
 
     return (
-        <TouchableOpacity
+        <TouchableOpacity style={styles.container}
             key={id}
             onPress={() => {
                 console.log('press')
             }}
-            style={{
-                width: '100%',
-                height: 100,
-                marginVertical: 6,
-                flexDirection: 'row',
-                alignItems: 'center',
-            }}>
-            <View
-                style={{
-                    width: '30%',
-                    height: 100,
-                    padding: 14,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backgroundColor: COLOURS.backgroundLight,
-                    borderRadius: 10,
-                    marginRight: 22,
-                }}>
-                <Image
-                    source={image}
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        resizeMode: 'contain',
-                    }}
-                />
+        >
+            <View style={styles.wrapper}>
+                <Image style={styles.icon} source={image} />
             </View>
-            <View
-                style={{
-                    flex: 1,
-                    height: '100%',
-                    justifyContent: 'space-around',
-                }}>
-                <View style={{}}>
-                    <Text
-                        style={{
-                            fontSize: 14,
-                            maxWidth: '100%',
-                            color: COLOURS.black,
-                            fontWeight: '600',
-                            letterSpacing: 1,
-                        }}>
-                        {name}
-                    </Text>
-                    <View
-                        style={{
-                            marginTop: 4,
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                            opacity: 0.6,
-                        }}>
-                        <Text
-                            style={{
-                                fontSize: 14,
-                                fontWeight: '400',
-                                maxWidth: '85%',
-                                marginRight: 4,
-                            }}>
-                            $ {price}
-                        </Text>
-                        <Text>
-                            ($ {2000})
-                        </Text>
+            <View style={styles.information}>
+                <View>
+                    <Text style={styles.title}>  {name} </Text>
+                    <View style={styles.detail}>
+                        <Text style={styles.price}> $ {price} </Text>
+                        <Text> ($ {2000}) </Text>
                     </View>
                 </View>
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                    }}>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                        }}>
-                        <View
-                            style={{
-                                borderRadius: 100,
-                                marginRight: 20,
-                                padding: 4,
-                                borderWidth: 1,
-                                borderColor: COLOURS.backgroundMedium,
-                                opacity: 0.5,
-                            }}>
-                            <MaterialCommunityIcons
-                                name="minus"
-                                style={{
-                                    fontSize: 16,
-                                    color: COLOURS.backgroundDark,
-                                }}
-                            />
+                <View style={styles.control}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', }}>
+                        <View style={styles.containerMinus}>
+                            <MaterialCommunityIcons name="minus" style={styles.minus} />
                         </View>
                         <Text>{cantidad}</Text>
-                        <View
-                            style={{
-                                borderRadius: 100,
-                                marginLeft: 20,
-                                padding: 4,
-                                borderWidth: 1,
-                                borderColor: COLOURS.backgroundMedium,
-                                opacity: 0.5,
-                            }}>
-                            <MaterialCommunityIcons
-                                name="plus"
-                                style={{
-                                    fontSize: 16,
-                                    color: COLOURS.backgroundDark,
-                                }}
-                            />
+                        <View style={styles.containerPlus}>
+                            <MaterialCommunityIcons name="plus" style={styles.plus} />
                         </View>
                     </View>
                     <TouchableOpacity onPress={() => { }}>
-                        <MaterialCommunityIcons
-                            name="delete-outline"
-                            style={{
-                                fontSize: 16,
-                                color: COLOURS.backgroundDark,
-                                backgroundColor: COLOURS.backgroundLight,
-                                padding: 8,
-                                borderRadius: 100,
-                            }}
-                        />
+                        <MaterialCommunityIcons style={styles.deleteButton} name="delete-outline" />
                     </TouchableOpacity>
                 </View>
             </View>
-            {/* <Text>{name}</Text > */}
         </TouchableOpacity>
     )
 }
 
 export default CardProducts
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 100,
+        marginVertical: 6,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    wrapper: {
+        width: '30%',
+        height: 100,
+        padding: 14,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLOURS.backgroundLight,
+        borderRadius: 10,
+        marginRight: 22,
+    },
+    icon: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
+    },
+    information: {
+        flex: 1,
+        height: '100%',
+        justifyContent: 'space-around',
+    },
+    title: {
+        fontSize: 14,
+        maxWidth: '100%',
+        color: COLOURS.black,
+        fontWeight: '600',
+        letterSpacing: 1,
+    },
+    detail: {
+        marginTop: 4,
+        flexDirection: 'row',
+        alignItems: 'center',
+        opacity: 0.6,
+    },
+    price: {
+        fontSize: 14,
+        fontWeight: '400',
+        maxWidth: '85%',
+        marginRight: 4,
+    },
+    control: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    containerMinus: {
+        borderRadius: 100,
+        marginRight: 20,
+        padding: 4,
+        borderWidth: 1,
+        borderColor: COLOURS.backgroundMedium,
+        opacity: 0.5,
+    },
+    containerPlus: {
+        borderRadius: 100,
+        marginLeft: 20,
+        padding: 4,
+        borderWidth: 1,
+        borderColor: COLOURS.backgroundMedium,
+        opacity: 0.5,
+    },
+    deleteButton: {
+        fontSize: 16,
+        color: COLOURS.backgroundDark,
+        backgroundColor: COLOURS.backgroundLight,
+        padding: 8,
+        borderRadius: 100,
+    },
+    plus: {
+        fontSize: 16,
+        color: COLOURS.backgroundDark,
+    },
+    minus: {
+        fontSize: 16,
+        color: COLOURS.backgroundDark,
+    }
+})
